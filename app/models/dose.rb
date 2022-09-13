aa521541cca2414c2261e3821cf88a0955766dc8
Daniel Domingo <domingo.daniel21@gmail.com>
2022-09-13 18:51:54 +0900

    almost finished

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :cocktail, uniqueness: { scope: :ingredient,
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id,
     message: "Should be unique!" }
 end
